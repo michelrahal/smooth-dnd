f1d8f6afd9d3698418af4ec6d1c2368ee674b689
Michel Rahal <michelrahal@gmail.com>
2023-10-20 13:59:04 +0200

    fix polyfills

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -20,27 +20,27 @@ function applyPolyfills() {
   // Overwrites native 'firstElementChild' prototype.
   // Adds Document & DocumentFragment support for IE9 & Safari.
   // Returns array instead of HTMLCollection.
-  (function (constructor: any) {
-    if (
-      constructor &&
-      constructor.prototype &&
-      constructor.prototype.firstElementChild == null
-    ) {
-      Object.defineProperty(constructor.prototype, "firstElementChild", {
-        get: function () {
-          var node,
-            nodes = this.childNodes,
-            i = 0;
-          while ((node = nodes[i++])) {
-            if (node.nodeType === 1) {
-              return node;
-            }
-          }
-          return null;
-        }
-      });
-    }
-  })(Node || Element);
+  // (function (constructor: any) {
+  //   if (
+  //     constructor &&
+  //     constructor.prototype &&
+  //     constructor.prototype.firstElementChild == null
+  //   ) {
+  //     Object.defineProperty(constructor.prototype, "firstElementChild", {
+  //       get: function () {
+  //         var node,
+  //           nodes = this.childNodes,
+  //           i = 0;
+  //         while ((node = nodes[i++])) {
+  //           if (node.nodeType === 1) {
+  //             return node;
+  //           }
+  //         }
+  //         return null;
+  //       }
+  //     });
+  //   }
+  // })(Node || Element);
 
   // Production steps of ECMA-262, Edition 5, 15.4.4.17
   // Reference: http://es5.github.io/#x15.4.4.17
